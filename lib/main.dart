6cdfffda1fe373ebe5019fc80100f792509a7261
eugen <eugen>
2021-07-06 19:50:39 +0200

    added DevicePreview package (for web platform only)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+import 'package:device_preview/device_preview.dart';
+
 import 'package:spot_out/screens/on_boarding/on_boarding_screen.dart';
 import 'package:spot_out/theme/app_themes.dart';
 
@@ -13,7 +17,24 @@ Future<void> main() async {
     ],
   );
 
-  runApp(const App());
+  Widget rootWidget;
+  if (kIsWeb) {
+    var devicePreview = DevicePreview(
+        enabled: true,
+        defaultDevice : Devices.ios.iPadMini,
+        plugins: [
+          //const ScreenshotPlugin(),
+          //const FileExplorerPlugin(),
+          //const SharedPreferencesExplorerPlugin(),
+        ],
+        builder: (context) => App()
+    );
+    rootWidget = devicePreview;
+  }
+  else {
+    rootWidget = new App();
+  }
+  runApp(rootWidget);
 }
 
 class App extends StatelessWidget {
